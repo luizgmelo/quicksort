48e40b94cae02b4033a0e03da129c28faa2602c6
luizgmelo <88911920+luizgmelo@users.noreply.github.com>
2023-03-21 08:42:10 -0300

    changed pivot for middle, less call stack

--- quicksort.js
+++ quicksort.js
@@ -1,7 +1,9 @@
 function quicksort(array) {
     if (array.length < 2) return array    
 
-    let pivot = array[0]    
+    
+    let middle = Math.floor((array.length - 1) / 2)
+    let pivot = array[middle]    
     let lowers = array.filter((element) => element < pivot) 
     let equals = array.filter((element) => element == pivot)
     let greaters = array.filter((element) => element > pivot)
@@ -14,18 +16,3 @@ console.log(quicksort([20]))
 console.log(quicksort([33, 15 ,10]))
 console.log(quicksort([33, 15, 10, 20, 25, 12, 13, 22]))
 
-
-// [15, 10] 33 pivot []
-// [10] 15 pivot []
-// [10]
-// [10,15]
-// [10, 15]
-// [10, 15, 33]
-
-// [33, 15, 10, 20, 25, 12, 13, 22]
-// [15, 10, 20, 25, 12, 13, 22] 33 pivot []
-// [10, 12, 13]  15 pivot  [20, 25, 22, 33]
-// [] 10 pivot [12,13]   | [] 20 pivot [25, 22 33]
-// [] 12 pivot [13]      | [22] 25 pivot [33]
-// [10, 12, 13, 15]      | [20, 22, 25, 33]
-// [10, 12, 13, 15, 20, 22, 25, 33]
