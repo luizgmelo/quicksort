eb2d8179c1b17ff1dfcd53f9a810bda9f46ab2af
Luiz Guilherme <luizgmelo_@hotmail.com>
2023-08-01 23:14:49 -0300

    added pivot start at middle index

--- quicksort.py
+++ quicksort.py
@@ -1,13 +1,17 @@
+from math import floor
+
 array = [30, 15, 5, 10]
 lowers = []
 greaters = []
 equals = []
+
 def quicksort(array):
 
     if len(array) < 2:
         return array 
-
-    pivot = array[0]
+    
+    middle = floor(len(array) - 1 / 2)
+    pivot = array[middle]
     
     lowers = list(filter(lambda x: x < pivot, array))
         
